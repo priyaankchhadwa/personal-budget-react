1a5988110c66ad2d23edccc76df11c274c73e332
priyaankchhadwa <priyaank29@gmail.com>
2020-10-16 18:29:09 -0400

    added files to react

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="bottom">
+            <div className="center">
+                All rights reserved &copy;Priyaank Chhadwa
+    </div>
+        </div>
+    );
+}
+
+export default Footer;
